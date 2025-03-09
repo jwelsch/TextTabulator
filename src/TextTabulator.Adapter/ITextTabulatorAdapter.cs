af8d5ffab9ff01385c7281d0f661fb6dc1a07d79
Justin Welsch <j.welsch@gmail.com>
2025-03-09 09:57:14 -0400

    Add ITextTabulatorAdapter interface

---
+++ src/TextTabulator.Adapter/ITextTabulatorAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TextTabulator.Adapter
+{
+    public interface ITextTabulatorAdapter
+    {
+        IEnumerable<string>? GetHeaderStrings();
+
+        IEnumerable<IEnumerable<string>> GetValueStrings();
+
+        IEnumerable<object>? GetHeaderObjects();
+
+        IEnumerable<IEnumerable<object>> GetValueObjects();
+
+        IEnumerable<CellValue>? GetHeaderCellValues();
+
+        IEnumerable<IEnumerable<CellValue>> GetValueCellValues();
+    }
+}
