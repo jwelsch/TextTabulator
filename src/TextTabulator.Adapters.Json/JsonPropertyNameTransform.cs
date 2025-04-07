ac202b8d37695bb14440a2ebc1afaba82866b184
Justin Welsch <j.welsch@gmail.com>
2025-04-07 09:38:30 -0400

    Add JsonTabulatorAdapterOptions class and IJsonPropertyNameTransform interface and implementations

---
+++ src/TextTabulator.Adapters.Json/JsonPropertyNameTransform.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+
+namespace TextTabulator.Adapters.Json
+{
+    /// <summary>
+    /// Interface for defining a transform for a JSON property name.
+    /// </summary>
+    public interface IJsonPropertyNameTransform
+    {
+        /// <summary>
+        /// Applies the transform to the property name.
+        /// </summary>
+        /// <param name="propertyName">Property name upon which to apply the tranform.</param>
+        /// <returns>The transformed name.</returns>
+        string Apply(string propertyName);
+    }
+
+    /// <summary>
+    /// A JSON property name transform that does not alter the property name.
+    /// </summary>
+    public class PassThruJsonPropertyNameTransform : IJsonPropertyNameTransform
+    {
+        public string Apply(string propertyName) => propertyName;
+    }
+
+    /// <summary>
+    /// A relatively simple JSON property name transform that can capitalize the first letter of words
+    /// and replace dashes.
+    /// </summary>
+    public class BasicJsonPropertyNameTransform : IJsonPropertyNameTransform
+    {
+        private readonly bool _capitalizeFirstLetterOfFirstWord;
+        private readonly bool _capitalizeFirstLetterOfSubsequentWords;
+        private readonly char? _dashReplacement;
+
+        /// <summary>
+        /// Creates an object of type BasicJsonPropertyNameTransform.
+        /// </summary>
+        /// <param name="capitalizeFirstLetterOfFirstWord">True to capitalize the first letter of the first word, false otherwise.</param>
+        /// <param name="capitalizeFirstLetterOfSubsequentWords">True to capitalize the first letter of subsequent words, false otherwise.</param>
+        /// <param name="dashReplacement">Specifies a character used to replace a dash. Pass in null to not replace a dash.</param>
+        public BasicJsonPropertyNameTransform(bool capitalizeFirstLetterOfFirstWord = true, bool capitalizeFirstLetterOfSubsequentWords = true, char? dashReplacement = ' ')
+        {
+            _capitalizeFirstLetterOfFirstWord = capitalizeFirstLetterOfFirstWord;
+            _capitalizeFirstLetterOfSubsequentWords = capitalizeFirstLetterOfSubsequentWords;
+            _dashReplacement = dashReplacement;
+        }
+
+        /// <summary>
+        /// Applies the transform to the property name.
+        /// </summary>
+        /// <param name="propertyName">Property name upon which to apply the tranform.</param>
+        /// <returns>The transformed name.</returns>
+        public string Apply(string propertyName)
+        {
+            var sb = new StringBuilder();
+            var firstLetter = true;
+            var firstWord = true;
+
+            for (var i = 0; i < propertyName.Length; i++)
+            {
+                var c = propertyName[i];
+
+                if (firstLetter)
+                {
+                    if ((c >= 'a' && c <= 'z')
+                        && ((firstWord && _capitalizeFirstLetterOfFirstWord) || (!firstWord && _capitalizeFirstLetterOfSubsequentWords)))
+                    {
+                        sb.Append(c + 32);
+                    }
+
+                    firstLetter = false;
+                }
+                else if (c == '-')
+                {
+                    sb.Append(_dashReplacement == null ? '-' : _dashReplacement);
+
+                    firstLetter = true;
+                    firstWord = false;
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ src/TextTabulator.Adapters.Json/JsonTabulatorAdapterOptions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace TextTabulator.Adapters.Json
+{
+    public class JsonTabulatorAdapterOptions
+    {
+        private readonly IJsonPropertyNameTransform _jsonPropertyNameTransform;
+        private readonly JsonReaderOptions _jsonReaderOptions;
+
+        public JsonTabulatorAdapterOptions(IJsonPropertyNameTransform? jsonPropertyNameTransform = default, JsonReaderOptions jsonReaderOptions = default)
+        {
+            _jsonPropertyNameTransform = jsonPropertyNameTransform ?? new PassThruJsonPropertyNameTransform();
+            _jsonReaderOptions = jsonReaderOptions;
+        }
+    }
+}
