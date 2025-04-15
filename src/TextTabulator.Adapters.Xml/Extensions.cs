46a34822b3eac2cb31727674df8cf3a91b174c6d
Justin Welsch <j.welsch@gmail.com>
2025-04-15 15:15:40 -0400

    Add extension method to XmlReader to skip whitespace or comments

---
+++ src/TextTabulator.Adapters.Xml/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml;
+
+namespace TextTabulator.Adapters.Xml
+{
+    [Flags]
+    internal enum XmlSkip : byte
+    {
+        None = 0x00,
+        Whitespace = 0x01,
+        Comment = 0x10
+    }
+
+    internal static class Extensions
+    {
+        internal static bool Read(this XmlReader xmlReader, XmlSkip skip)
+        {
+            while (xmlReader.Read())
+            {
+                if (xmlReader.NodeType == XmlNodeType.Whitespace)
+                {
+                    if ((skip & XmlSkip.Whitespace) == 0)
+                    {
+                        return true;
+                    }
+                }
+                else if (xmlReader.NodeType == XmlNodeType.Comment)
+                {
+                    if ((skip & XmlSkip.Comment) == 0)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
