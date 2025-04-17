63676ef0d4360f697d58cb967fd241378c9f74af
Justin Welsch <j.welsch@gmail.com>
2025-04-17 10:39:44 -0400

    Move version scripts to build directory

--- change-versions.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [Parameter(Position=0, HelpMessage="Which version to change.")]
-    [string] $FromVersion,
-
-    [Parameter(Position=1, HelpMessage="What the new version should be.")]
-    [string] $ToVersion
-)
-
-.\version.ps1 change $FromVersion $ToVersion -Directory .\src -Recurse
--- list-versions.ps1
+++
@@ -1,1 +0,0 @@
-.\version.ps1 list -Directory .\src -Recurse
