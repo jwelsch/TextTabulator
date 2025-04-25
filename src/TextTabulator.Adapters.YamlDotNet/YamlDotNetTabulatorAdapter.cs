d1aceec2dcab80e60ff77b012b77ce082590a12f
Justin Welsch <j.welsch@gmail.com>
2025-04-25 12:37:22 -0400

    Add core implementation of YamlDotNetTabulatorAdapter using deserializer

--- src/TextTabulator.Adapters.YamlDotNet/YamlDotNetTabulatorAdapter.cs
+++ src/TextTabulator.Adapters.YamlDotNet/YamlDotNetTabulatorAdapter.cs
@@ -1,20 +1,184 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using YamlDotNet.Serialization;
+
 namespace TextTabulator.Adapters.YamlDotNet
 {
-    public interface IYamlDotNetTabulatorAdapter : ITabulatorAdapter
+    /// <summary>
+    /// Public interface for IYamlDotNetTabulatorAdapter.
+    /// </summary>
+    public interface IYamlDotNetTabulatorAdapter : ITabulatorAdapter, IDisposable
     {
     }
 
+    /// <summary>
+    /// The adapter class that accepts YAML data and presents the data that it reads in a format that TextTabulator.Tabulate can consume.
+    /// 
+    /// The data should be in the following format:
+    /// 
+    /// - field1: value1A
+    ///   field2: value2A
+    /// - field1: value1B
+    ///   field2: value2B
+    /// - field1: value1C
+    ///   field2: value2C
+    ///   
+    /// </summary>
     public class YamlDotNetTabulatorAdapter : IYamlDotNetTabulatorAdapter
     {
+        private readonly Deserializer _yamlDotNetDeserializer;
+        private readonly TextReader _data;
+        private readonly bool _shouldDispose;
+        private readonly YamlDotNetTabulatorAdapterOptions _options;
+        private readonly Dictionary<string, TableHeader> _headers = new();
+        private readonly IValueNormalizer _valueNormalizer = new ValueNormalizer();
+
+        private IList<object>? _deserialized;
+        private bool _disposed;
+
+        public YamlDotNetTabulatorAdapter(Deserializer yamlDotNetDeserializer, StringProvider dataProvider, YamlDotNetTabulatorAdapterOptions? options = null)
+            : this(yamlDotNetDeserializer, new StringReader(dataProvider.Invoke()), options)
+        {
+            _shouldDispose = true;
+        }
+
+        public YamlDotNetTabulatorAdapter(Deserializer yamlDotNetDeserializer, TextReader dataProvider, YamlDotNetTabulatorAdapterOptions? options = null)
+        {
+            _yamlDotNetDeserializer = yamlDotNetDeserializer;
+            _data = dataProvider;
+            _options = options ?? new YamlDotNetTabulatorAdapterOptions();
+        }
+
+        public YamlDotNetTabulatorAdapter(Deserializer yamlDotNetDeserializer, string inputData, YamlDotNetTabulatorAdapterOptions? options = null)
+            : this(yamlDotNetDeserializer, () => inputData, options)
+        {
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed here.
+
+                    if (_shouldDispose)
+                    {
+                        _data.Dispose();
+                    }
+                }
+
+                // Dispose unmanaged here.
+
+                _disposed = true;
+            }
+        }
+
         public IEnumerable<string>? GetHeaderStrings()
         {
-            throw new NotImplementedException();
+            _headers.Clear();
+            _deserialized?.Clear();
+
+            var headers = new List<string>();
+
+            _deserialized = (IList<object>?)_yamlDotNetDeserializer.Deserialize(_data);
+
+            if (_deserialized == null)
+            {
+                return null;
+            }
+
+            if (_deserialized.Count < 1)
+            {
+                return Array.Empty<string>();
+            }
+
+            var items = (IDictionary<object, object>?)_deserialized[0];
+
+            if (items == null)
+            {
+                return Array.Empty<string>();
+            }
+
+            var column = 0;
+
+            foreach (var item in items)
+            {
+                _headers.Add(item.Key?.ToString() ?? string.Empty, new TableHeader(_options.NodeNameTransform.Apply(item.Value?.ToString() ?? string.Empty), column++));
+            }
+
+            return items?.Select(i => i.Key.ToString() ?? string.Empty);
         }
 
         public IEnumerable<IEnumerable<string>> GetValueStrings()
         {
-            throw new NotImplementedException();
+            if (_deserialized == null || _deserialized.Count < 1)
+            {
+                return Array.Empty<IEnumerable<string>>();
+            }
+
+            var rowValues = new List<string[]>();
+
+            for (var i = 0; i < _deserialized.Count; i++)
+            {
+                var row = (IDictionary<object, object>?)_deserialized[i] ?? throw new InvalidOperationException($"Deserialized row with index '{i}' was null.");
+
+                if (row.Count > _headers.Count)
+                {
+                    throw new InvalidOperationException($"The number of values '{row.Count}' in row with index '{i}' is greater than the number of headers '{_headers.Count}'.");
+                }
+
+                var col = 0;
+                var values = new string[_headers.Count];
+                Array.Fill(values, "");
+
+                foreach (var value in row)
+                {
+                    var header = value.Key.ToString() ?? throw new InvalidOperationException($"Deserialized row value with row index '{i}' and column index '{col}' was null.");
+
+                    if (!_headers.TryGetValue(header, out var tableHeader))
+                    {
+                        throw new InvalidOperationException($"Unknown header '{header}' encountered while parsing values.");
+                    }
+
+                    var type = value.Value?.GetType();
+
+                    if ((type?.IsClass ?? false) && type != typeof(string))
+                    {
+                        if (type == typeof(List<object>))
+                        {
+                            values[tableHeader.Index] = "<YAML Array>";
+                        }
+                        else if (type == typeof(Dictionary<object, object>))
+                        {
+                            values[tableHeader.Index] = "<YAML Object>";
+                        }
+                        else
+                        {
+                            values[tableHeader.Index] = $"<Unknown data type '{type}'>";
+                        }
+                    }
+                    else
+                    {
+                        values[tableHeader.Index] = _valueNormalizer.Normalize($"{value.Value}");
+                    }
+
+                    col++;
+                }
+
+                rowValues.Add(values);
+            }
+
+            return rowValues;
         }
     }
 }
--- tests/TextTabulator.Adapters.YamlDotNetTests/YamlDotNetTabulatorAdapterTests.cs
+++ tests/TextTabulator.Adapters.YamlDotNetTests/YamlDotNetTabulatorAdapterTests.cs
@@ -1,11 +1,474 @@
-﻿namespace TextTabulator.Adapters.YamlDotNetTests
+﻿using System.Text;
+using TextTabulator.Adapters.YamlDotNet;
+using YamlDotNet.Serialization;
+
+namespace TextTabulator.Adapters.YamlDotNetTests
 {
     public class YamlDotNetTabulatorAdapterTests
     {
-        [Fact]
-        public void Test1()
-        {
+        #region YAML Data
 
+        private readonly static string YamlWithSingleSimpleObject =
+"""
+- name: Tyrannosaurus Rex
+  weight: 6.7
+  diet: Carnivore
+  extinction: 66
+  test:
+""";
+
+        private readonly static string YamlWithMultipleSimpleObjects =
+"""
+- name: Tyrannosaurus Rex
+  weight: 6.7
+  diet: Carnivore
+  extinction: 66
+  test: 
+- name: Triceratops
+  weight: 8
+  diet: Herbivore
+  extinction: 66
+  test: null
+- name: Archaeopteryx
+  weight: 0.001
+  diet: Omnivore
+  extinction: 147
+  test: null
+""";
+
+        private readonly static string YamlWithSingleComplexObject =
+"""
+- name: Tyrannosaurus Rex
+  weight: 6.7
+  diet: Carnivore
+  extinction: 66
+  formations:
+    - Hell Creek
+    - Lance
+    - North Horn
+    - Javelina
+  bipedal: true
+  teeth:
+    shape: conical
+    length: 30
+    serrated: true
+    count: 60
+  test: null
+""";
+
+        private readonly static string YamlWithMultipleComplexObjects =
+"""
+- name: Tyrannosaurus Rex
+  weight: 6.7
+  diet: Carnivore
+  extinction: 66
+  formations:
+    - Hell Creek
+    - Lance
+    - North Horn
+    - Javelina
+  bipedal: true
+  teeth:
+    shape: conical
+    length: 30
+    serrated: true
+    count: 60
+  test: null
+- name: Triceratops
+  weight: 8
+  diet: Herbivore
+  extinction: 66
+  formations:
+    - Evanston
+    - Scollard
+    - Laramie
+    - Lance
+    - Denver
+    - Hell Creek
+  bipedal: false
+  teeth:
+    shape: battery
+    length: 4.5
+    serrated: false
+    count: 800
+  test: null
+- name: Archaeopteryx
+  weight: 0.001
+  diet: Omnivore
+  extinction: 147
+  formations:
+    - Solnhofen Limestone
+  bipedal: true
+  teeth:
+    shape: conical
+    length: 0.1
+    serrated: false
+    count: 8
+  test: null
+""";
+
+        private readonly static string YamlWithObjectsWithAnExtraProperty =
+"""
+- name: Tyrannosaurus Rex
+  weight: 6.7
+  diet: Carnivore
+  extinction: 66
+- name: Triceratops
+  weight: 8
+  diet: Herbivore
+  extinction: 66
+  bipedal: false
+""";
+
+        private readonly static string YamlWithObjectsMissingAProperty =
+"""
+- name: Tyrannosaurus Rex
+  weight: 6.7
+  diet: Carnivore
+  extinction: 66
+- name: Triceratops
+  weight: 8
+  diet: Herbivore
+""";
+
+        private readonly static string YamlWithObjectsWithOutOfOrderProperties =
+"""
+- name: Tyrannosaurus Rex
+  weight: 6.7
+  diet: Carnivore
+  extinction: 66
+- diet: Herbivore
+  name: Triceratops
+  extinction: 66
+  weight: 8
+""";
+
+        #endregion
+
+        [Fact]
+        public void When_yaml_is_single_simple_object_then_headers_returned()
+        {
+            var deserializer = new Deserializer();
+
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, YamlWithSingleSimpleObject);
+            var headers = sut.GetHeaderStrings();
+
+            Assert.NotNull(headers);
+            Assert.Collection(headers,
+                i => Assert.Equal("name", i),
+                i => Assert.Equal("weight", i),
+                i => Assert.Equal("diet", i),
+                i => Assert.Equal("extinction", i),
+                i => Assert.Equal("test", i)
+            );
+        }
+
+        [Fact]
+        public void When_yaml_is_single_simple_object_then_values_returned()
+        {
+            var deserializer = new Deserializer();
+
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, YamlWithSingleSimpleObject);
+
+            _ = sut.GetHeaderStrings();
+            var values = sut.GetValueStrings();
+
+            Assert.NotNull(values);
+            Assert.Collection(values, i =>
+            {
+                Assert.NotNull(i);
+                Assert.Collection(i,
+                    j => Assert.Equal("Tyrannosaurus Rex", j),
+                    j => Assert.Equal("6.7", j),
+                    j => Assert.Equal("Carnivore", j),
+                    j => Assert.Equal("66", j),
+                    j => Assert.Equal("", j)
+                );
+            });
+        }
+
+        [Fact]
+        public void When_yaml_is_single_complex_object_then_headers_returned()
+        {
+            var deserializer = new Deserializer();
+
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, YamlWithSingleComplexObject);
+
+            var headers = sut.GetHeaderStrings();
+
+            Assert.NotNull(headers);
+            Assert.Collection(headers,
+                i => Assert.Equal("name", i),
+                i => Assert.Equal("weight", i),
+                i => Assert.Equal("diet", i),
+                i => Assert.Equal("extinction", i),
+                i => Assert.Equal("formations", i),
+                i => Assert.Equal("bipedal", i),
+                i => Assert.Equal("teeth", i),
+                i => Assert.Equal("test", i)
+            );
+        }
+
+        [Fact]
+        public void When_yaml_is_single_complex_object_then_values_returned()
+        {
+            var deserializer = new Deserializer();
+
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, YamlWithSingleComplexObject);
+
+            _ = sut.GetHeaderStrings();
+            var values = sut.GetValueStrings();
+
+            Assert.NotNull(values);
+            Assert.Collection(values, i =>
+            {
+                Assert.NotNull(i);
+                Assert.Collection(i,
+                    j => Assert.Equal("Tyrannosaurus Rex", j),
+                    j => Assert.Equal("6.7", j),
+                    j => Assert.Equal("Carnivore", j),
+                    j => Assert.Equal("66", j),
+                    j => Assert.Equal("<YAML Array>", j),
+                    j => Assert.Equal("True", j),
+                    j => Assert.Equal("<YAML Object>", j),
+                    j => Assert.Equal("", j)
+                );
+            });
+        }
+
+        [Fact]
+        public void When_yaml_is_multiple_simple_objects_then_headers_returned()
+        {
+            var deserializer = new Deserializer();
+
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, YamlWithMultipleSimpleObjects);
+
+            var headers = sut.GetHeaderStrings();
+
+            Assert.NotNull(headers);
+            Assert.Collection(headers,
+                i => Assert.Equal("name", i),
+                i => Assert.Equal("weight", i),
+                i => Assert.Equal("diet", i),
+                i => Assert.Equal("extinction", i),
+                i => Assert.Equal("test", i)
+            );
+        }
+
+        [Fact]
+        public void When_yaml_is_multiple_simple_objects_then_values_returned()
+        {
+            var deserializer = new Deserializer();
+
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, YamlWithMultipleSimpleObjects);
+
+            _ = sut.GetHeaderStrings();
+            var values = sut.GetValueStrings();
+
+            Assert.NotNull(values);
+            Assert.Collection(values,
+                i => {
+                    Assert.NotNull(i);
+                    Assert.Collection(i,
+                        j => Assert.Equal("Tyrannosaurus Rex", j),
+                        j => Assert.Equal("6.7", j),
+                        j => Assert.Equal("Carnivore", j),
+                        j => Assert.Equal("66", j),
+                        j => Assert.Equal("", j)
+                    );
+                },
+                i => {
+                    Assert.NotNull(i);
+                    Assert.Collection(i,
+                        j => Assert.Equal("Triceratops", j),
+                        j => Assert.Equal("8", j),
+                        j => Assert.Equal("Herbivore", j),
+                        j => Assert.Equal("66", j),
+                        j => Assert.Equal("", j)
+                    );
+                },
+                i => {
+                    Assert.NotNull(i);
+                    Assert.Collection(i,
+                        j => Assert.Equal("Archaeopteryx", j),
+                        j => Assert.Equal("0.001", j),
+                        j => Assert.Equal("Omnivore", j),
+                        j => Assert.Equal("147", j),
+                        j => Assert.Equal("", j)
+                    );
+                });
+        }
+
+        [Fact]
+        public void When_yaml_is_multiple_complex_objects_then_values_returned()
+        {
+            var deserializer = new Deserializer();
+
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, YamlWithMultipleComplexObjects);
+
+            _ = sut.GetHeaderStrings();
+            var values = sut.GetValueStrings();
+
+            Assert.NotNull(values);
+            Assert.Collection(values,
+                i => {
+                    Assert.NotNull(i);
+                    Assert.Collection(i,
+                        j => Assert.Equal("Tyrannosaurus Rex", j),
+                        j => Assert.Equal("6.7", j),
+                        j => Assert.Equal("Carnivore", j),
+                        j => Assert.Equal("66", j),
+                        j => Assert.Equal("<YAML Array>", j),
+                        j => Assert.Equal("True", j),
+                        j => Assert.Equal("<YAML Object>", j),
+                        j => Assert.Equal("", j)
+                    );
+                },
+                i => {
+                    Assert.NotNull(i);
+                    Assert.Collection(i,
+                        j => Assert.Equal("Triceratops", j),
+                        j => Assert.Equal("8", j),
+                        j => Assert.Equal("Herbivore", j),
+                        j => Assert.Equal("66", j),
+                        j => Assert.Equal("<YAML Array>", j),
+                        j => Assert.Equal("False", j),
+                        j => Assert.Equal("<YAML Object>", j),
+                        j => Assert.Equal("", j)
+                    );
+                },
+                i => {
+                    Assert.NotNull(i);
+                    Assert.Collection(i,
+                        j => Assert.Equal("Archaeopteryx", j),
+                        j => Assert.Equal("0.001", j),
+                        j => Assert.Equal("Omnivore", j),
+                        j => Assert.Equal("147", j),
+                        j => Assert.Equal("<YAML Array>", j),
+                        j => Assert.Equal("True", j),
+                        j => Assert.Equal("<YAML Object>", j),
+                        j => Assert.Equal("", j)
+                    );
+                });
+        }
+
+        [Fact]
+        public void When_yaml_is_objects_with_an_extra_property_then_throw()
+        {
+            var deserializer = new Deserializer();
+
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, YamlWithObjectsWithAnExtraProperty);
+
+            _ = sut.GetHeaderStrings();
+
+            Action action = () => sut.GetValueStrings();
+
+            Assert.Throws<InvalidOperationException>(action);
+        }
+
+        [Fact]
+        public void When_yaml_is_objects_missing_a_property_then_values_returned()
+        {
+            var deserializer = new Deserializer();
+
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, YamlWithObjectsMissingAProperty);
+
+            _ = sut.GetHeaderStrings();
+            var values = sut.GetValueStrings();
+
+            Assert.NotNull(values);
+            Assert.Collection(values,
+                i => {
+                    Assert.NotNull(i);
+                    Assert.Collection(i,
+                        j => Assert.Equal("Tyrannosaurus Rex", j),
+                        j => Assert.Equal("6.7", j),
+                        j => Assert.Equal("Carnivore", j),
+                        j => Assert.Equal("66", j)
+                    );
+                },
+                i => {
+                    Assert.NotNull(i);
+                    Assert.Collection(i,
+                        j => Assert.Equal("Triceratops", j),
+                        j => Assert.Equal("8", j),
+                        j => Assert.Equal("Herbivore", j),
+                        j => Assert.Equal("", j)
+                    );
+                });
+        }
+
+        [Fact]
+        public void When_yaml_is_objects_with_out_of_order_properties_then_values_returned()
+        {
+            var deserializer = new Deserializer();
+
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, YamlWithObjectsWithOutOfOrderProperties);
+
+            _ = sut.GetHeaderStrings();
+            var values = sut.GetValueStrings();
+
+            Assert.NotNull(values);
+            Assert.Collection(values,
+                i => {
+                    Assert.NotNull(i);
+                    Assert.Collection(i,
+                        j => Assert.Equal("Tyrannosaurus Rex", j),
+                        j => Assert.Equal("6.7", j),
+                        j => Assert.Equal("Carnivore", j),
+                        j => Assert.Equal("66", j)
+                    );
+                },
+                i => {
+                    Assert.NotNull(i);
+                    Assert.Collection(i,
+                        j => Assert.Equal("Triceratops", j),
+                        j => Assert.Equal("8", j),
+                        j => Assert.Equal("Herbivore", j),
+                        j => Assert.Equal("66", j)
+                    );
+                });
+        }
+
+        [Fact]
+        public void When_yaml_is_from_stringprovider_then_headers_returned()
+        {
+            StringProvider provider = () => YamlWithSingleSimpleObject;
+
+            var deserializer = new Deserializer();
+
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, provider);
+
+            var headers = sut.GetHeaderStrings();
+
+            Assert.NotNull(headers);
+            Assert.Collection(headers,
+                i => Assert.Equal("name", i),
+                i => Assert.Equal("weight", i),
+                i => Assert.Equal("diet", i),
+                i => Assert.Equal("extinction", i),
+                i => Assert.Equal("test", i)
+            );
+        }
+
+        [Fact]
+        public void When_yaml_is_from_textreader_then_headers_returned()
+        {
+            using var stream = new MemoryStream(UTF8Encoding.UTF8.GetBytes(YamlWithSingleSimpleObject));
+            using var reader = new StreamReader(stream);
+            var deserializer = new Deserializer();
+            var sut = new YamlDotNetTabulatorAdapter(deserializer, reader);
+
+            var headers = sut.GetHeaderStrings();
+
+            Assert.NotNull(headers);
+            Assert.Collection(headers,
+                i => Assert.Equal("name", i),
+                i => Assert.Equal("weight", i),
+                i => Assert.Equal("diet", i),
+                i => Assert.Equal("extinction", i),
+                i => Assert.Equal("test", i)
+            );
         }
     }
 }
