2d94b5efd35f14db0aa7433c553a44ad45f064b4
Justin Welsch <j.welsch@gmail.com>
2025-04-25 12:34:30 -0400

    Pull value normalization into a separate class

---
+++ src/TextTabulator.Adapters/ValueNormalizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TextTabulator.Adapters
+{
+    public interface IValueNormalizer
+    {
+        string Normalize(string value);
+    }
+
+    public class ValueNormalizer : IValueNormalizer
+    {
+        public string Normalize(string value)
+        {
+            if (value.Equals("true", StringComparison.OrdinalIgnoreCase))
+            {
+                return "True";
+            }
+            else if (value.Equals("false", StringComparison.OrdinalIgnoreCase))
+            {
+                return "False";
+            }
+
+            return value;
+        }
+    }
+}
