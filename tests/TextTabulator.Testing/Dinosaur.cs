e5b367df3d369cc367d5921dc3e8fb3e0cfeeb64
Justin Welsch <j.welsch@gmail.com>
2025-07-17 10:23:16 -0400

    Add testing for TextTabulator.Adapters.MLDotNet

---
+++ tests/TextTabulator.Testing/Dinosaur.cs
@@ -0,0 +1,32 @@
+﻿namespace TextTabulator.Testing
+{
+    public interface IDinosaur
+    {
+        string Name { get; }
+
+        double Weight { get; }
+
+        string Diet { get; }
+
+        int ExtinctionMya { get; }
+    }
+
+    public class Dinosaur : IDinosaur
+    {
+        public string Name { get; }
+
+        public double Weight { get; }
+
+        public string Diet { get; }
+
+        public int ExtinctionMya { get; }
+
+        public Dinosaur(string name, double weight, string diet, int extinctionMya)
+        {
+            Name = name;
+            Weight = weight;
+            Diet = diet;
+            ExtinctionMya = extinctionMya;
+        }
+    }
+}
