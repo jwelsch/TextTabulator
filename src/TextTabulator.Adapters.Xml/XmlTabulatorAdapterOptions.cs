0777f17fd3b2eae77c7fe9244201c6a8ad504efc
Justin Welsch <j.welsch@gmail.com>
2026-02-25 11:24:49 -0500

    Add header sorting to XmlTabulatorAdapter

--- src/TextTabulator.Adapters.Xml/XmlTabulatorAdapterOptions.cs
+++ src/TextTabulator.Adapters.Xml/XmlTabulatorAdapterOptions.cs
@@ -18,14 +18,21 @@ namespace TextTabulator.Adapters.Xml
         public XmlReaderSettings XmlReaderSettings { get; }
 
         /// <summary>
+        /// Gets the sorter to use for the headers.
+        /// </summary>
+        public IHeaderOrderSorter HeaderSorter { get; }
+
+        /// <summary>
         /// Creates an object of type XmlTabulatorAdapterOptions.
         /// </summary>
         /// <param name="nodeNameTransform">Transform to apply to XML node names. Passing null will cause the XML node names to not be altered.</param>
         /// <param name="xmlReaderSettings">Specifies a set of features to support on the XmlReader object created by the Create method. Passing null will use an XmlReaderSettings object with default values.</param>
-        public XmlTabulatorAdapterOptions(INameTransform? nodeNameTransform = null, XmlReaderSettings? xmlReaderSettings = null)
+        /// <param name="headerSorter">Specifies the sorter to use for the headers. Passing null will use the default header sorter.</param>
+        public XmlTabulatorAdapterOptions(INameTransform? nodeNameTransform = null, XmlReaderSettings? xmlReaderSettings = null, IHeaderOrderSorter? headerSorter = null)
         {
             NodeNameTransform = nodeNameTransform ?? new PassThruNameTransform();
             XmlReaderSettings = xmlReaderSettings ?? new XmlReaderSettings();
+            HeaderSorter = headerSorter ?? HeaderOrderSorter.Default;
         }
     }
 }
